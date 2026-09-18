bc2fb3da0042f2e6e1546611c114b68ad3ca25e1
andzain <artistwalks@protonmail.com>
2026-09-18 18:13:38 +0400

    add sitemap and 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,29 @@
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://vahram.aghasyan.net',
-  integrations: [svelte()],
+  site: "https://va.021920.xyz",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "hy"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  integrations: [
+    svelte(),
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en-US",
+          hy: "hy-AM",
+        },
+      },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
